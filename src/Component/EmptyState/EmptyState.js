ddf73606be3bc9d8900d257e78d84b7fe601993f
Mrinmay <devmrin@gmail.com>
2018-08-27 02:00:37 +0530

    update style - empty state body

--- src/Component/EmptyState/EmptyState.js
+++ src/Component/EmptyState/EmptyState.js
@@ -8,8 +8,8 @@ const EmptyState = () => (
     <h1 className="emptyHeading">so quiet...</h1>
     <img src={NoList} alt="no-lists" />
     <h2 className="instructionHeading">
-      Click on the " <i className="fas fa-plus" /> " icon on the top-right to
-      create a new list.
+      Click on the " <i className="fas fa-plus" /> " icon on the{' '}
+      <b>top-right</b> to create a new list.
     </h2>
   </div>
 );
