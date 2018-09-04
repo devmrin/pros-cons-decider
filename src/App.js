0c0254a984d8a6f70f7114be9d817173b998b77c
Mrinmay <devmrin@gmail.com>
2018-09-04 07:56:24 +0530

    added logic for base firestore loading - new base - v0.3

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <HomePage />
+        <HomePage lists={this.state.lists} />
       </div>
     );
   }
